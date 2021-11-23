975e03aa909723cb2cf215f08ddc9363265f07fb
Emily Love Mills <emily@emlove.me>
2021-11-23 09:01:22 -0600

    Support for Python 3.9

--- setup.py
+++ setup.py
@@ -26,9 +26,9 @@ setup(
         'License :: OSI Approved :: Apache Software License',
         'Natural Language :: English',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
     ],
     description=(
         "Async library to control Zerproc Bluetooth LED smart string lights"),
